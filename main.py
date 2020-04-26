fc1209906b1c0ea8605627cab9f0303349d6170f
Jose <jrs3@gatech.edu>
2020-04-26 09:42:54 -0400

    indent update

--- main.py
+++ main.py
@@ -29,32 +29,35 @@ import threading
 path= list()# global list so that we can keep track of the paths we are on
 
 
-#------------------------------------------------------------------------------------------
-# initialize the video stream and allow the camera sensor to warm up
-print("starting video stream..")
-#vs = VideoStream(src=0).start()# this is for testing with a web cam not in use. We didn't have anymore equipment
-vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)# 
 
-#------------------------------------------------------------------------------------------
 
 
 #define a function that will capture video for us 
 
 def imageRec():
+    #------------------------------------------------------------------------------------------
+    # initialize the video stream and allow the camera sensor to warm up
+    print("starting video stream..")
+    #vs = VideoStream(src=0).start()# this is for testing with a web cam not in use. We didn't have anymore equipment
+    vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)# 
 
-	#wrapping it in a try loop incase the camera fails
-	try:
-		while True:
-			# grab the frame from the threaded video stream and resize it to  have a maximum width of 400 pixels
-			frame = vs.read()
-			frame = imutils.resize(frame, width=400)#this is strickly for debug the monitoring the actual device would not have a screen
-			# find the barcodes in the frame and decode each of the barcodes
+    #------------------------------------------------------------------------------------------
+    #wrapping it in a try loop incase the camera fails
+    try:
+        print("try")
+        while True:
+            print("while")
+        
+            # grab the frame from the threaded video stream and resize it to  have a maximum width of 400 pixels
+            frame = vs.read()
+            frame = imutils.resize(frame, width=400)#this is strickly for debug the monitoring the actual device would not have a screen
+            # find the barcodes in the frame and decode each of the barcodes
             barcodes = pyzbar.decode(frame)
             # loop over the detected barcodes
             for barcode in barcodes:
-            	print("hello")
-            	# extract the bounding box location of the barcode and draw
+                print("hello")
+                # extract the bounding box location of the barcode and draw
                 # the bounding box surrounding the barcode on the image
                 (x, y, w, h) = barcode.rect
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -65,31 +68,29 @@ def imageRec():
                 # draw the barcode data and barcode type on the image
                 text = "{} ({})".format(barcodeData, barcodeType)
                 cv2.putText(frame, text, (x, y - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                if BarcideData not in path:
-                	path.append(BarcideData)
-                	print("type: ", type(BarcideData),"this is the actual data: ",BarcideData)
+                if barcodeData not in path:
+                        path.append(barcodeData)
+                        print("type: ", type(barcodeData),"this is the actual data: ",barcodeData)
 
 
             #this is just to put the window where we want it for testing                        
             windowName="Robot Cam"
             cv2.namedWindow(windowName)#name the window
-            cv2.moveWindow(windowName,1200,10)#move and create the window
+            #cv2.moveWindow(windowName,1200,10)#move and create the window
             cv2.imshow(windowName, frame)#display our window
 
+    except KeyboardInterrupt:
+        print("cleaning up...")
+        cv2.destroyAllWindows()
+        vs.stop()#this kills the camera
 
-	
-	except KeyboardInterrupt:
-		print("cleaning up...")
-	    cv2.destroyAllWindows()
-    	vs.stop()#this kills the camera
-    	destroy()
-
+imageRec()
 #create threads
-videoThread = threading.Thread(target=imageRec) 
+#videoThread = threading.Thread(target=imageRec) 
 
 
 #starting the thread
-videoThread.start()
+#videoThread.start()
 
 
 
